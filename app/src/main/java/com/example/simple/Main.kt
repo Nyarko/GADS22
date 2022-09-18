f903185a1ba99ce27425b3530ebd976f3a4bcc99
Nyarko <nyarko.agyekum@yahoo.com>
2022-09-18 23:19:34 +0000

    Commented out code for intro section. Moving to next section(of the Course).

--- app/src/main/java/com/example/simple/Main.kt
+++ app/src/main/java/com/example/simple/Main.kt
@@ -1,25 +1,28 @@
-package com.example.simple
 
-import com.example.simple.com.rsk.Person
 
-fun main (args: Array<String>) {
-    println("Hello World")
-
-    val kevin = Person("Kevin")
-    //kevin.Name = "Olion"
-
-    println("Your name is ${kevin.Name}")
-
-    kevin.Name ="Steven"
-    println("Your name is ${kevin.Name}")
-
-    kevin.display()
-
-    kevin.displayWithLambda(::printName)
-
-    //kevin = Person()
-}
-
-fun printName(name: String) {
-    println(name)
-}
+//FOR SECTION INTRODUCTION IN KOTLIN FUNDAMENTALS
+//package com.example.simple
+//
+//import com.example.simple.com.rsk.Person
+//
+//fun main (args: Array<String>) {
+//    println("Hello World")
+//
+//    val kevin = Person("Kevin")
+//    //kevin.Name = "Olion"
+//
+//    println("Your name is ${kevin.Name}")
+//
+//    kevin.Name ="Steven"
+//    println("Your name is ${kevin.Name}")
+//
+//    kevin.display()
+//
+//    kevin.displayWithLambda(::printName)
+//
+//    //kevin = Person()
+//}
+//
+//fun printName(name: String) {
+//    println(name)
+//}
